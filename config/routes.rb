28da8adf5f2c8aae084b764abaa0c5f639f0bdf4
Matthew Nguyen <mnguyen916@gmail.com>
2012-05-18 19:20:05 -0500

    Finish sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 AuctionApp::Application.routes.draw do
-  resources :users
+  get "sessions/create"
 
+  get "sessions/destroy"
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
   
   match '/signup', to: 'users#new'
+
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
